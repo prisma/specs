8652b6e95fd571d47691d1a71855c5d5cd81664a
Kuldar <hi@kuldar.com>
2019-07-10 13:43:44 +0200

    Add P6, Command component

--- cli/src/components/cli/index.ts
+++ cli/src/components/cli/index.ts
@@ -4,9 +4,10 @@ import Title from './Title'
 import Spacer from './Spacer'
 import Button from './Button'
 import Line from './Line'
+import Command from './Command'
 import Checkbox from './Checkbox'
 import Success from './Success'
 import TextInput from './TextInput'
 import Introspection from './Introspection'
 
-export { Terminal, Title, SectionTitle, Spacer, Button, Line, Checkbox, Success, TextInput, Introspection }
+export { Terminal, Title, SectionTitle, Spacer, Button, Line, Checkbox, Success, TextInput, Introspection, Command }
