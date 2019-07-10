035f3d6908990808916928b301f1f83c0c37ccaa
Kuldar <hi@kuldar.com>
2019-07-10 11:25:23 +0200

    Add Success message component

--- cli/src/components/cli/index.ts
+++ cli/src/components/cli/index.ts
@@ -4,5 +4,6 @@ import Spacer from './Spacer'
 import Button from './Button'
 import Line from './Line'
 import Checkbox from './Checkbox'
+import Success from './Success'
 
-export { Terminal, SectionTitle, Spacer, Button, Line, Checkbox }
+export { Terminal, SectionTitle, Spacer, Button, Line, Checkbox, Success }
