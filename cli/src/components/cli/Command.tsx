6d6e998c029d59ac452feabd8acd906f8d452795
Kuldar <hi@kuldar.com>
2019-07-11 18:17:02 +0200

    Refactor Command component

--- cli/src/components/cli/Command.tsx
+++ cli/src/components/cli/Command.tsx
@@ -2,13 +2,28 @@ import React from 'react'
 import colors from '../../colors'
 
 // Command
-export default ({ children }) => (
-  <div>
-    $ <span style={commandStyles}>{children}</span>
+export default ({ label, description, labelWidth = 'auto', noPrefix = false  }) => (
+  <div style={commandStyles}>
+    {!noPrefix && <span>$&nbsp;</span>}
+    <div style={labelStyles(labelWidth)}>{label}</div>
+    <div style={descriptionStyles}>{description}</div>
   </div>
 )
 
 // Styles
 const commandStyles = {
-  color: colors.whiteBright
+  display: 'flex',
+  alignItems: 'center'
+}
+
+const labelStyles = labelWidth => {
+  return {
+    color: colors.whiteBright,
+    width: labelWidth
+  }
+}
+
+const descriptionStyles = {
+  color: colors.blackBright,
+  marginLeft: 8
 }
