6a8e3371a25be82757f9e98cec3cff50bc143b2f
schickling <schickling.j@gmail.com>
2019-07-16 18:47:30 +0200

    fix build

--- cli/src/utils/generateNavLinks.ts
+++ cli/src/utils/generateNavLinks.ts
@@ -1,11 +1,15 @@
-const generateNavLinks = (pagesData) => {
+const generateNavLinks = pagesData => {
   const navLinks = pagesData.reduce((links, page) => {
-    if (page.node.context.frontmatter !== null && page.node.context.frontmatter.navLabel !== null) {
+    if (
+      page.node.context &&
+      page.node.context.frontmatter &&
+      page.node.context.frontmatter.navLabel
+    ) {
       const link = {
         url: page.node.path,
         category: page.node.context.frontmatter.navGroup,
         label: page.node.context.frontmatter.navLabel,
-        order: page.node.context.frontmatter.navOrder
+        order: page.node.context.frontmatter.navOrder,
       }
       links[link.category] = links[link.category] || []
       links[link.category].push(link)
