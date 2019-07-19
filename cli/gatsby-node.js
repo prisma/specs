8c7a422872f3232b130fc0fbad03ef0ab21a0a00
schickling <schickling.j@gmail.com>
2019-07-19 11:47:09 +0200

    clean build

--- cli/gatsby-node.js
+++ cli/gatsby-node.js
@@ -1,8 +1,6 @@
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage, deletePage } = actions
 
-  console.log(page)
-
   deletePage(page)
   // You can access the variable "house" in your page queries now
   createPage({
